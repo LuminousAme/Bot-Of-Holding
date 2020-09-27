ba54bd1f15df2876b7693f443b6ae306be19c1d7
Primal System Studio <primalsystemstudio@protonmail.com>
2020-09-27 04:21:38 +0400

    Filled out all bot commands
    Filled out all bot commands, not functional yet.
    Filled out database access
    new "add_user" function

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
-#import dotenv library
-from dotenv import load_dotenv
+#import logging library
+import logging
 
 #import os library
 import os
@@ -13,6 +13,8 @@ import json
 #import the discord library 
 import discord
 
+#import dotenv library
+from dotenv import load_dotenv
 #import the commands extension 
 from discord.ext import commands
 #import the get extension
@@ -20,8 +22,14 @@ from discord.utils import get
 
 
 #load bot secrets
-loaddotenv()
-TOKEN = os.getenv(BOT_TOKEN)
+load_dotenv()
+TOKEN = os.getenv('BOT_TOKEN')
+
+#set up logging file
+logging.basicConfig(filename='log.txt', filemode='a', 
+                    format='%(asctime)s %(msecs)d- %(process)d-%(levelname)s - %(message)s', 
+                    datefmt='%d-%b-%y %H:%M:%S %p' ,
+                    level=logging.DEBUG)
 
 #set the command prefix for the bot 
 client = commands.Bot(command_prefix = 'boh;')
@@ -38,50 +46,144 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         await ctx.send('That command does not exist.')
 
-#generic function for accessing database
-def open_database(**kwargs):
-    conn = sqlite3.connect('boh.db')
+#generic functions for adding users
+def add_user(user, *arg):
+    connec = sqlite3.connect('boh.db')
+    cursor = conn.cursor()
+    if len(arg) > 0:
+        inv = json.dump({arg})
+    else:
+        inv = json.dump({})
+    to_insert = (user, inv,)
+    cursor.execute('INSERT INTO boh VALUES (?,?)', to_insert)
+    #Save changes then close the database    
+    connec.commit()
+    connec.close()
+    print(user + " added to database.")
+    logging.info(user + " added to database.")
+
+#generic function for accessing & modifying inventories
+def access_db(command,author,**kwargs):
+    connec = sqlite3.connect('boh.db')
     cursor = conn.cursor()
 
-    #If inserting into inventory
-    #If removing from inventory
-    
-    #Save changes then close the database    
-    conn.commit()
-    conn.close()
+    #find author in database
+    if cursor.execute('SELECT Inventory FROM boh WHERE UserID=?', (author,)):
+        if (command == 'give') or (command == 'discard'):
+            inv = json.load(cursor.fetchone())
+            #check if item in author inventory
+            if kwargs['item'] in inv:
+                #check if there is enough to remove
+                if inv[kwargs['item']] >= kwargs['amount']:
+                    #remove item from author inventory 
+                    inv[kwargs['item']] = inv[kwargs['item']] - kwargs['amount']
+                    #if amount of items is reduced to 0, remove it completely
+                    if inv[kwargs['item']] == 0:
+                        inv.pop(kwargs['item'])
+                    # Update inventory
+                    inv = json.dump(inv)
+                    cursor.execute('UPDATE bot SET Inventory = ? WHERE UserID=?', (inv, author,))
+                    #Save changes then close the database    
+                    connec.commit()
+                    connec.close()
+                else:
+                    return "wrong_amount"
+            else:
+                return "not_in"
+            #insert item into to_give[user] inventory
+            if command == 'give':
+                #check if user in database
+                if cursor.execute('SELECT Inventory FROM boh WHERE UserID=?',
+                        (author,)):
+                    inv = json.load(cursor.fetchone())
+                     #check if item in user inventory
+                    if kwargs['item'] in inv:
+                        # Increase amount of items
+                        inv[kwargs['item']] == inv[kwargs['item']] + kwargs['amount']
+                    else:
+                        # Add item to inventory
+                        inv.update({kwargs['item']: kwargs['amount']})
+                        inv = json.dump(inv)
+                # If not in database, add the user
+                else:
+                    inv = json.dump({kwargs['item']: kwargs['amount']})
+                    add_user(kwargs['user'], inv)
+        elif command == 'inventory':
+            #return inventory
+            return cursor.fetchone()
+        elif command == 'empty':
+            cursor.execute('UPDATE boh SET Inventory = "{}" WHERE UserID=?',
+                                (author,))
+            #Save changes then close the database    
+            connec.commit()
+            connec.close()
+            return 'success_empty'
+    #if author doesn't exist make a new entry
+    else:
+        add_user(kwargs['user'])
 
 #******Bag of holding commands******
 #give command
 @client.command(aliases=['g', 'G', 'give'])
-async def Give(ctx, user: str, item: str, amount: int):
+async def Give(ctx, user: discord.User, item: str, amount: int):
     # Check if user exists
-    # Check if author has item in the right amount
-    # Transfer item from author record to user record
+    ##NEED TO FIX HOW TO CHECK FOR DISCORD USER
+    if guild.member(discord.User):
+        print('hello')
+        guild = ctx.guild
+        author = ctx.author.id
+        to_give = {'user': discord.Member.id, 
+                        'item': item, 'amount': amount}
+        # If user exists, transfer the item   
+        access_db('give',author,to_give)
+        ##NEED TO ADD WHAT TO DO WITH ERRORS
+    else:
+        await ctx.send(user + " is not in this server.")
+        print(user + " is not in this server.")
+        logging.info(user + " is not in this server.")
 
 #list command
 @client.command(aliases=['inventory', 'Inventory', 'list', 'l', 'L', 'i', 'I'])
 async def List(ctx):
     # Find author
+    author = ctx.author.id
+    inv = access_db('inventory', author)
     # List items attached to author in inventory
-
+    if (inv != 1) or (inv != []):
+        for item in inventory:
+            await ctx.send(item)
+    else:
+        await ctx.send("You don't anything in your inventory.")
 
 #use command
 @client.command(aliases=['u', 'U', 'use'])
 async def Use(ctx, user: str, item: str):
-    pass
+    #discard only 1 of item
+    to_discard = {'user': user.id, 'item': item, 'amount': 1}
+    access_db('discard', to_discard)
+    ##ADD WHAT DO DO WITH ERRORS
 
 #discard
 @client.command(aliases=['d', 'D', 'discard'])
 async def Discard(ctx, item: str, amount: int):
-    # Find author
-    # Find item in author inventory
-    # Remove 'amount' of items from inventory
+    author = ctx.author.id
+    print(author + "")
+    # Discard amount of item from user inventory
+    result = access_db('discard')
+    ##ADD WHAT TO DO WITH ERRORS
+        
 
 #empty
 @client.command(aliases=['e', 'E', 'empty'])
 async def Empty(ctx):
-    # Find author
-    # Revert inventory to {}
+    # Check they actually want to do this
+    message = await ctx.send("Are you sure that you want to empty your inventory?")
+    ##ADD REACTIONS
+    ##ADD WHAT TO DO WITH ADDED REACTIONS
+    def check(reaction, user):
+        return user == ctx.author and str(reaction.emoji)
+    ##ADD WHAT TO DO IF THEY SAY YES
+    ##ADD WHAT TO DO WITH ERRORS
 
 #allow the code to run on discord 
 client.run(TOKEN)
